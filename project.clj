0809569df50ed295b266724d257f9c28921b6c99
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-06-30 16:49:19 +0200

    project: bump to 1.7!

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject spootnik/unilog "0.7.5"
+(defproject spootnik/unilog "0.7.6"
   :description "logging should be easy!"
   :url "https://github.com/pyr/unilog"
   :license {:name "MIT License"
             :url "https://github.com/pyr/unilog/tree/master/LICENSE"}
   :plugins [[codox "0.8.10"]]
   :codox {:defaults {:doc/format :markdown}}
-  :dependencies [[org.clojure/clojure                           "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure                           "1.7.0"]
                  [net.logstash.logback/logstash-logback-encoder "4.2"]
                  [org.slf4j/slf4j-api                           "1.7.12"]
                  [org.slf4j/log4j-over-slf4j                    "1.7.12"]
