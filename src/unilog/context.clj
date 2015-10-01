43f0244a80cedfd837d71d7cad40e81d80053e2e
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-10-01 10:05:47 +0200

    context: functions to interact with Mapped Diagnostic Context

---
+++ src/unilog/context.clj
@@ -0,0 +1,26 @@
+(ns unilog.context
+  "Provide a way to interact with Mapped Diagnostic Context")
+
+(defn push-context
+  "Add a key to the current Mapped Diagnostic Context"
+  [k v]
+  (org.slf4j.MDC/put (name k) (str v)))
+
+(defn pull-context
+  "Remove a key to the current Mapped Diagnostic Context"
+  [k]
+  (org.slf4j.MDC/remove (name k)))
+
+(defmacro with-context
+  "Execute body with the Mapped Diagnostic Context updated from
+   keys found in the ctx map."
+  [ctx & body]
+  `(if-not (map? ~ctx)
+     (throw (ex-info "with-context expects a map" {}))
+     (try
+       (doseq [[k# v#] ~ctx ]
+         (push-context k# v#))
+       ~@body
+       (finally
+         (doseq [[k# _] ~ctx]
+           (pull-context k#))))))
