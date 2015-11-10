3551a1c69d14ecf3fd11d4b47d66438cef499475
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-11-10 20:17:28 +0100

    project: bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/unilog "0.7.10"
+(defproject spootnik/unilog "0.7.11"
   :description "logging should be easy!"
   :url "https://github.com/pyr/unilog"
   :license {:name "MIT License"
