f984543aba29a2c3a217adfb26ebe62ed2557047
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-03-30 08:39:59 +0200

    project: bump clojure

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://github.com/pyr/unilog/tree/master/LICENSE"}
   :plugins [[codox "0.8.10"]]
   :codox {:defaults {:doc/format :markdown}}
-  :dependencies [[org.clojure/clojure                           "1.7.0"]
+  :dependencies [[org.clojure/clojure                           "1.8.0"]
                  [net.logstash.logback/logstash-logback-encoder "4.2"]
                  [org.slf4j/slf4j-api                           "1.7.12"]
                  [org.slf4j/log4j-over-slf4j                    "1.7.12"]
