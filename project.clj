03821889f6bc521d29164d881092398c8a8c43d9
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-12-23 08:22:17 +0100

    project: prepare for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/unilog "0.7.16"
+(defproject spootnik/unilog "0.7.17-SNAPSHOT"
   :description "logging should be easy!"
   :url "https://github.com/pyr/unilog"
   :license {:name "MIT License"
