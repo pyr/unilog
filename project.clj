7d6db1f68cd008273312469997cee61b9dc8cc4a
Coda Hale <coda.hale@gmail.com>
2016-12-22 17:46:02 -0800

    Upgrade dependencies.

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/pyr/unilog"
   :license {:name "MIT License"
             :url "https://github.com/pyr/unilog/tree/master/LICENSE"}
-  :plugins [[lein-codox "0.9.6"]]
+  :plugins [[lein-codox "0.10.2"]]
   :codox {:defaults {:doc/format :markdown}}
   :dependencies [[org.clojure/clojure                           "1.8.0"]
-                 [net.logstash.logback/logstash-logback-encoder "4.7"]
-                 [org.slf4j/slf4j-api                           "1.7.21"]
-                 [org.slf4j/log4j-over-slf4j                    "1.7.21"]
-                 [org.slf4j/jul-to-slf4j                        "1.7.21"]
-                 [ch.qos.logback/logback-classic                "1.1.7"]
-                 [ch.qos.logback/logback-core                   "1.1.7"]]
+                 [net.logstash.logback/logstash-logback-encoder "4.8"]
+                 [org.slf4j/slf4j-api                           "1.7.22"]
+                 [org.slf4j/log4j-over-slf4j                    "1.7.22"]
+                 [org.slf4j/jul-to-slf4j                        "1.7.22"]
+                 [ch.qos.logback/logback-classic                "1.1.8"]
+                 [ch.qos.logback/logback-core                   "1.1.8"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.3.1"]]}})
