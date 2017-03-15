063d7cd16ab7f3eb842cc411c99a3a79fcdc57d8
Coda Hale <coda.hale@gmail.com>
2017-03-15 15:19:50 -0700

    Upgrade to logback 1.2.x. (#20)

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
   :codox {:source-uri "https://github.com/pyr/unilog/blob/{version}/{filepath}#L{line}"
           :metadata   {:doc/format :markdown}}
   :dependencies [[org.clojure/clojure                           "1.8.0"]
-                 [net.logstash.logback/logstash-logback-encoder "4.8"]
-                 [org.slf4j/slf4j-api                           "1.7.22"]
-                 [org.slf4j/log4j-over-slf4j                    "1.7.22"]
-                 [org.slf4j/jul-to-slf4j                        "1.7.22"]
-                 [ch.qos.logback/logback-classic                "1.1.8"]
-                 [ch.qos.logback/logback-core                   "1.1.8"]]
+                 [net.logstash.logback/logstash-logback-encoder "4.9"]
+                 [org.slf4j/slf4j-api                           "1.7.24"]
+                 [org.slf4j/log4j-over-slf4j                    "1.7.24"]
+                 [org.slf4j/jul-to-slf4j                        "1.7.24"]
+                 [ch.qos.logback/logback-classic                "1.2.1"]
+                 [ch.qos.logback/logback-core                   "1.2.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.3.1"]
                                   [cheshire                  "5.7.0"]
                                   [clj-time                  "0.13.0"]]
