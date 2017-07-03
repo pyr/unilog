ac03d4ffe22900af4d4b5058062fe45c39105069
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-07-03 09:04:26 +0200

    release 0.7.21

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/unilog "0.7.21-SNAPSHOT"
+(defproject spootnik/unilog "0.7.21"
   :description "logging should be easy!"
   :url "https://github.com/pyr/unilog"
   :license {:name "MIT License"
@@ -8,12 +8,12 @@
           :metadata   {:doc/format :markdown}}
   :dependencies [[org.clojure/clojure                           "1.8.0"]
                  [net.logstash.logback/logstash-logback-encoder "4.9"]
-                 [org.slf4j/slf4j-api                           "1.7.24"]
-                 [org.slf4j/log4j-over-slf4j                    "1.7.24"]
-                 [org.slf4j/jul-to-slf4j                        "1.7.24"]
-                 [ch.qos.logback/logback-classic                "1.2.1"]
-                 [ch.qos.logback/logback-core                   "1.2.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.3.1"]
-                                  [cheshire                  "5.7.0"]
+                 [org.slf4j/slf4j-api                           "1.7.25"]
+                 [org.slf4j/log4j-over-slf4j                    "1.7.25"]
+                 [org.slf4j/jul-to-slf4j                        "1.7.25"]
+                 [ch.qos.logback/logback-classic                "1.2.3"]
+                 [ch.qos.logback/logback-core                   "1.2.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.4.0"]
+                                  [cheshire                  "5.7.1"]
                                   [clj-time                  "0.13.0"]]
                    :global-vars {*warn-on-reflection* true}}})
