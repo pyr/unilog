e3e9cd7f0a16579d98f7a726370f1fbfb40bf10d
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-12-19 10:13:02 +0100

    project: bump deps

--- project.clj
+++ project.clj
@@ -3,17 +3,18 @@
   :url "https://github.com/pyr/unilog"
   :license {:name "MIT License"
             :url "https://github.com/pyr/unilog/tree/master/LICENSE"}
-  :plugins [[lein-codox "0.10.3"]]
+  :plugins [[lein-codox "0.10.3"]
+            [lein-ancient "0.6.15"]]
   :codox {:source-uri "https://github.com/pyr/unilog/blob/{version}/{filepath}#L{line}"
           :metadata   {:doc/format :markdown}}
-  :dependencies [[org.clojure/clojure                           "1.8.0"]
-                 [net.logstash.logback/logstash-logback-encoder "4.9"]
+  :dependencies [[org.clojure/clojure                           "1.9.0"]
+                 [net.logstash.logback/logstash-logback-encoder "4.11"]
                  [org.slf4j/slf4j-api                           "1.7.25"]
                  [org.slf4j/log4j-over-slf4j                    "1.7.25"]
                  [org.slf4j/jul-to-slf4j                        "1.7.25"]
                  [ch.qos.logback/logback-classic                "1.2.3"]
                  [ch.qos.logback/logback-core                   "1.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.4.0"]
-                                  [cheshire                  "5.7.1"]
-                                  [clj-time                  "0.13.0"]]
+                                  [cheshire                  "5.8.0"]
+                                  [clj-time                  "0.14.2"]]
                    :global-vars {*warn-on-reflection* true}}})
