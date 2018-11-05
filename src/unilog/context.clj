5ef0a21f2dbc559f112d3f83548fc06a63c69457
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-11-05 17:15:34 +0100

    fixup! fixup! fixup! context: mdc-bound-fn

--- src/unilog/context.clj
+++ src/unilog/context.clj
@@ -40,9 +40,10 @@
 
 (comment
 
-  ;; Example usage
+  ;; Example usage, you'll need a pattern that shows MDC values
 
   (require '[clojure.tools.logging :refer [info]])
+
   (let [f (mdc-fn [] (with-context {:mdcval "bar"} (info "something")))]
     @(future
        (f))))
