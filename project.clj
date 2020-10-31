27570b38b1f10c95d1d45fcc8c4ede441e3eecbd
Pierre-Yves Ritschard <pyr@spootnik.org>
2020-10-31 17:46:49 +0100

    Version 0.7.26

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/unilog "0.7.26-SNAPSHOT"
+(defproject spootnik/unilog "0.7.26"
   :description "logging should be easy!"
   :url "https://github.com/pyr/unilog"
   :license {:name "MIT License"
