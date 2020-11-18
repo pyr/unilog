d2caefc05bf2d0443bf9ad7426538bd8b5b6ff47
Pierre-Yves Ritschard <pyr@spootnik.org>
2020-11-18 13:16:00 +0100

    project: ensure linting during CI

--- project.clj
+++ project.clj
@@ -2,9 +2,8 @@
   :description "logging should be easy!"
   :url "https://github.com/pyr/unilog"
   :license {:name "MIT License"
-            :url "https://github.com/pyr/unilog/tree/master/LICENSE"}
-  :plugins [[lein-ancient "0.6.15"]
-            [lein-cljfmt "0.6.7"]]
+            :url  "https://github.com/pyr/unilog/tree/master/LICENSE"}
+  :plugins [[lein-ancient "0.6.15"]]
   :pedantic? :abort
   :dependencies [[org.clojure/clojure                           "1.10.1"]
                  [net.logstash.logback/logstash-logback-encoder "6.4"]
@@ -14,7 +13,15 @@
                  [ch.qos.logback/logback-classic                "1.2.3"]
                  [ch.qos.logback/logback-core                   "1.2.3"]]
   :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.logging "1.1.0"]
-                                  [metosin/jsonista          "0.2.7"
-                                   :exclusions [com.fasterxml.jackson.core/*]]]
-                   :global-vars {*warn-on-reflection* true}}})
+  :profiles {:dev  {:dependencies [[org.clojure/tools.logging "1.1.0"]
+                                   [metosin/jsonista          "0.2.7"
+                                    :exclusions [com.fasterxml.jackson.core/*]]]
+                    :pedantic?    :ignore
+                    :plugins      [[lein-ancient "0.6.15"]]
+                    :global-vars  {*warn-on-reflection* true}}
+             :test {:dependencies [[org.clojure/tools.logging "1.1.0"]
+                                   [metosin/jsonista          "0.2.7"
+                                    :exclusions [com.fasterxml.jackson.core/*]]]
+                    :plugins      [[lein-difftest "2.0.0"]
+                                   [lein-cljfmt "0.6.7"]]
+                    :pedantic?    :abort}})
