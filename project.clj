f745bb0632c522626901877d4f218d09ce5fa5d6
Arnaud Geiser <arnaud.geiser@exoscale.ch>
2021-10-06 14:27:16 +0200

    project: support jul-over-slf4j & bump dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
+(def slfj4-version "1.7.32")
+(def logback-version "1.2.6")
+
 (defproject spootnik/unilog "0.7.28-SNAPSHOT"
   :description "logging should be easy!"
   :url "https://github.com/pyr/unilog"
@@ -5,23 +8,24 @@
             :url  "https://github.com/pyr/unilog/tree/master/LICENSE"}
   :plugins [[lein-ancient "0.6.15"]]
   :pedantic? :abort
-  :dependencies [[org.clojure/clojure                           "1.10.1"]
-                 [net.logstash.logback/logstash-logback-encoder "6.4"]
-                 [org.slf4j/slf4j-api                           "1.7.30"]
-                 [org.slf4j/log4j-over-slf4j                    "1.7.30"]
-                 [org.slf4j/jul-to-slf4j                        "1.7.30"]
-                 [ch.qos.logback/logback-classic                "1.2.3"]
-                 [ch.qos.logback/logback-core                   "1.2.3"]]
+  :dependencies [[org.clojure/clojure                           "1.10.3"]
+                 [net.logstash.logback/logstash-logback-encoder "6.6"]
+                 [org.slf4j/slf4j-api                           ~slfj4-version]
+                 [org.slf4j/log4j-over-slf4j                    ~slfj4-version]
+                 [org.slf4j/jul-to-slf4j                        ~slfj4-version]
+                 [org.slf4j/jcl-over-slf4j                      ~slfj4-version]
+                 [ch.qos.logback/logback-classic                ~logback-version]
+                 [ch.qos.logback/logback-core                   ~logback-version]]
   :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
   :profiles {:dev  {:dependencies [[org.clojure/tools.logging "1.1.0"]
-                                   [metosin/jsonista          "0.2.7"
+                                   [metosin/jsonista          "0.3.4"
                                     :exclusions [com.fasterxml.jackson.core/*]]]
                     :pedantic?    :ignore
-                    :plugins      [[lein-ancient "0.6.15"]]
+                    :plugins      [[lein-ancient "0.7.0"]]
                     :global-vars  {*warn-on-reflection* true}}
              :test {:dependencies [[org.clojure/tools.logging "1.1.0"]
-                                   [metosin/jsonista          "0.2.7"
+                                   [metosin/jsonista          "0.3.4"
                                     :exclusions [com.fasterxml.jackson.core/*]]]
                     :plugins      [[lein-difftest "2.0.0"]
-                                   [lein-cljfmt "0.6.7"]]
+                                   [lein-cljfmt "0.8.0"]]
                     :pedantic?    :abort}})
