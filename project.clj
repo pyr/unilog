731a7d9bbc00d4f1739d04142e7f8c6c472eadb0
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-05-28 14:04:35 +0200

    project: bump dependencies. fixes #30 and CVE-2020-36518

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(def slf4j-version "1.7.32")
-(def logback-version "1.2.8")
+(def slf4j-version "1.7.36")
+(def logback-version "1.2.11")
 
 (defproject spootnik/unilog "0.7.30-SNAPSHOT"
   :description "logging should be easy!"
@@ -8,8 +8,8 @@
             :url  "https://github.com/pyr/unilog/tree/master/LICENSE"}
   :plugins [[lein-ancient "0.7.0"]]
   :pedantic? :abort
-  :dependencies [[org.clojure/clojure                           "1.10.3"]
-                 [net.logstash.logback/logstash-logback-encoder "6.6"]
+  :dependencies [[org.clojure/clojure                           "1.11.1"]
+                 [net.logstash.logback/logstash-logback-encoder "7.2"]
                  [org.slf4j/slf4j-api                           ~slf4j-version]
                  [org.slf4j/log4j-over-slf4j                    ~slf4j-version]
                  [org.slf4j/jul-to-slf4j                        ~slf4j-version]
@@ -17,14 +17,14 @@
                  [ch.qos.logback/logback-classic                ~logback-version]
                  [ch.qos.logback/logback-core                   ~logback-version]]
   :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
-  :profiles {:dev  {:dependencies [[org.clojure/tools.logging "1.1.0"]
+  :profiles {:dev  {:dependencies [[org.clojure/tools.logging "1.2.4"]
                                    [metosin/jsonista          "0.3.4"
                                     :exclusions [com.fasterxml.jackson.core/*]]]
                     :pedantic?    :ignore
                     :plugins      [[lein-ancient "0.7.0"]]
                     :global-vars  {*warn-on-reflection* true}}
-             :test {:dependencies [[org.clojure/tools.logging "1.1.0"]
-                                   [metosin/jsonista          "0.3.4"
+             :test {:dependencies [[org.clojure/tools.logging "1.2.4"]
+                                   [metosin/jsonista          "0.3.5"
                                     :exclusions [com.fasterxml.jackson.core/*]]]
                     :plugins      [[lein-difftest "2.0.0"]
                                    [lein-cljfmt "0.8.0"]]
